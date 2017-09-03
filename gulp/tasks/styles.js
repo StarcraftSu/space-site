2caebc39c9f67cbf4069d590379108a327fff592
StarcraftSu <starcraftsu@gmail.com>
2017-09-03 22:01:39 +0800

    img switch for mobile devices

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,11 @@ var postcss=require("gulp-postcss");                //载入postcss
 var autoprefixer=require("autoprefixer");           //自动根据caniuse来生成兼容的CSS代码
 var cssvars=require("postcss-simple-vars");         //用来把变量用在CSS中
 var nested=require('postcss-nested');				//使代码可以寄宿，工整CSS开发
-var cssImport=require('postcss-import');   			//需要在postcss中第一个引入
-
+var cssImport=require('postcss-import');		   //需要在postcss中第一个引入
+var mixins=require('postcss-mixins');
 gulp.task('styles',function(){
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
+	.pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nested]))
 	.on('error',function(errorMessage){
 		console.log(errorMessage.toString);
 		this.emit('end');
