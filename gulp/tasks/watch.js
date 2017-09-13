e26d359b9f1e3f307e266e36fb84730729a8d5b6
StarcraftSu <starcraftsu@gmail.com>
2017-09-13 22:45:29 +0800

    error fixing

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,11 +18,8 @@ gulp.task('watch',function(){
 		gulp.start('cssInject');
 	});
 
-<<<<<<< HEAD
-	watch('./app/assets/script/**/*',function(){
-=======
+
 	watch('./app/assets/scripts/**/*',function(){
->>>>>>> 7620d92224108d6b9b593e33c64c9e6b2f72bb97
 		gulp.start('jsInject');
 	})
 });
@@ -36,8 +33,5 @@ gulp.task('cssInject',['styles'],function(){
 gulp.task('jsInject',['scripts'],function(){
 	return gulp.src('./app/temp/scripts/app.js')
 	.pipe(browserSync.stream());
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> 7620d92224108d6b9b593e33c64c9e6b2f72bb97
