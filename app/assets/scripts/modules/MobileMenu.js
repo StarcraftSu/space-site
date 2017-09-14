01cc9d8fa32da6e1cd9c4463023b7b987862cd0c
StarcraftSu <starcraftsu@gmail.com>
2017-09-14 22:06:17 +0800

    header-icon done

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -4,6 +4,7 @@ class MobileMenu {
 
 	constructor(){
 	//获取DOM对象
+		this.siteHeader = $('.site-header');
 		this.menuIcon = $('.site-header__menu-icon');
 		this.menuContent = $('.site-header__menu-content');
 		this.events();
@@ -12,14 +13,15 @@ class MobileMenu {
 	//监听动作
 	events(){
 		this.menuIcon.click(this.toggleTheMenu.bind(this));	
+		 // this.menuIcon.on('click',this.toggleTheMenu);	
 		
 	}
 
 	//具体方法
 	toggleTheMenu(){
-		
 		this.menuContent.toggleClass('site-header__menu-content--is-visible');
-
+		this.siteHeader.toggleClass('site-header--is-expanded');
+		this.menuIcon.toggleClass('site-header__menu-icon--close-x');
 	}
 
 }
--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -104,6 +104,7 @@ var MobileMenu = function () {
 		_classCallCheck(this, MobileMenu);
 
 		//获取DOM对象
+		this.siteHeader = (0, _jquery2.default)('.site-header');
 		this.menuIcon = (0, _jquery2.default)('.site-header__menu-icon');
 		this.menuContent = (0, _jquery2.default)('.site-header__menu-content');
 		this.events();
@@ -116,6 +117,7 @@ var MobileMenu = function () {
 		key: 'events',
 		value: function events() {
 			this.menuIcon.click(this.toggleTheMenu.bind(this));
+			// this.menuIcon.on('click',this.toggleTheMenu);	
 		}
 
 		//具体方法
@@ -123,8 +125,9 @@ var MobileMenu = function () {
 	}, {
 		key: 'toggleTheMenu',
 		value: function toggleTheMenu() {
-
 			this.menuContent.toggleClass('site-header__menu-content--is-visible');
+			this.siteHeader.toggleClass('site-header--is-expanded');
+			this.menuIcon.toggleClass('site-header__menu-icon--close-x');
 		}
 	}]);
 
