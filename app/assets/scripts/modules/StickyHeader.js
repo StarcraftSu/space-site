cff53ce4a48c2cdae42697afb6b8ddd2720879ef
StarcraftSu <starcraftsu@gmail.com>
2017-09-27 17:19:47 +0800

    complete lazyloading

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -3,6 +3,7 @@ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoi
 import smoothScroll from 'jquery-smooth-scroll';//平滑滑动到选中区域
 class StickyHeader{
 	constructor(){
+		this.lazyImages = $('.lazyload');
 		this.siteHeader = $('.site-header');
 		this.headerTrigger = $('.large-hero__title');
 		this.createHeaderWaypoint();
@@ -10,7 +11,16 @@ class StickyHeader{
 		this.headerLinks = $('.primary-nav a');
 		this.createPageSectionWaypoints();
 		this.addSmoothScrolling();
+		this.refreshWaypoints();
 	}
+
+
+	refreshWaypoints(){
+		this.lazyImages.on('load',function(){
+			Waypoint.refreshAll();
+		});
+	}
+
 
 	addSmoothScrolling(){
 		this.headerLinks.smoothScroll();
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ var path = require('path');   //npm 内置的绝对路径
 
 
 module.exports = {
-	entry:"./app/assets/scripts/app.js",
+	entry:{
+		App:"./app/assets/scripts/app.js",
+		Vendor:"./app/assets/scripts/vendor.js"
+	},
 	output:{
 		path:path.resolve(__dirname,"./app/temp/scripts"),
-		filename:"app.js"
+		filename:"[name].js"
 	},
 	module:{
 		loaders:[
